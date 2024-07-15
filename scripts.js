f438b335149c3217ea098583dd75eb0aff6f9e65
Mynul Islam <mynul.sakil@gmail.com>
2024-07-15 07:25:34 +0600

    fix icons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-// scripts.js
+console.log('scripts.js loaded');
 document.getElementById('get-started').addEventListener('click', function() {
     alert('Getting started with PDF tools!');
 });
+document.getElementById('menu-btn').addEventListener('click', function() {
+    document.getElementById('hamburger-menu').classList.toggle('hidden');
+})
